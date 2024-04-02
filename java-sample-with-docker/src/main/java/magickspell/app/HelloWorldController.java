605a48ab8882ec63820daa46f3a871e4af31809c
a.solomchenko <a.solomchenko@brusnika.ru>
2024-04-02 08:12:26 +0300

    root dir

--- java-sample-with-docker/src/main/java/magickspell/app/HelloWorldController.java
+++ java-sample-with-docker/src/main/java/magickspell/app/HelloWorldController.java
@@ -18,7 +18,7 @@ public class HelloWorldController {
 
   @GetMapping("/hello")
   public String helloWorld() {
-    return "Hello, World! 07";
+    return "Hello, World! 007";
   }
 
   @GetMapping("/hello/user")
